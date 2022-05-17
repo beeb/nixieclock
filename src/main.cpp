fbe2bf274026d01d9fb4ca0aa005946cb8d2badd
Valentin Bersier <vbersier@gmail.com>
2022-05-17 19:02:37 +0200

    work in progress

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <SPI.h>
 #include <WiFi.h>
 #include "time.h"
 
@@ -5,30 +6,15 @@
 #define PIN_DIN 13
 #define PIN_CLK 14
 #define PIN_OE 27
-#define SHIFT_LSB_FIRST false
-#define BUFSIZE 12
-#define PRESCALER 15
-#define MAXBRIGHTNESS 10
-#define MAXIMUM_LUX 100
 
 const char *ssid = "***REMOVED***";
 const char *wifipw = "***REMOVED***";
 
 struct tm timeInfo;
-int maxDigits = 6;
-String driverSetupStr;
-hw_timer_t *ESP32timer = NULL;
-unsigned long intCounter = 0;
-portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
-bool displayON = true;
-int lx = 0;
-int prevSecond = 0;
-int prevMinute = 0;
-int prevHour = 0;
-boolean showDate = false;
-boolean showClock = false;
-bool initProtectionTimer = false;
-time_t protectTimer;
+time_t prevTime = 0;
+
+String stringToDisplay = "000000";
+unsigned int symbolArray[10] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512}; // 0 to 9
 
 byte digitPins[6][10] = {
     {130, 121, 122, 123, 124, 125, 126, 127, 128, 129}, // sec  1
@@ -38,33 +24,6 @@ byte digitPins[6][10] = {
     {20, 11, 12, 13, 14, 15, 16, 17, 18, 19},           // hour  1
     {10, 1, 2, 3, 4, 5, 6, 7, 8, 9}                     // hour 10
 };
-
-uint32_t DRAM_ATTR bitBuffer0 = 0; // new digits
-uint32_t DRAM_ATTR bitBuffer1 = 0;
-uint32_t DRAM_ATTR oldBitBuffer0 = 0; // old digits
-uint32_t DRAM_ATTR oldBitBuffer1 = 0;
-int DRAM_ATTR animPtr = 0;
-
-// Brightness PWM timing. Greater value => slower brightness PWM frequency
-//  Suggested values: 100000 / 20000, for faster PWM: 50000 / 10000
-uint32_t DRAM_ATTR PWMrefresh = 100000;
-uint32_t DRAM_ATTR PWM_min = 6000;
-
-uint32_t DRAM_ATTR time1 = 2000;
-uint32_t DRAM_ATTR time2 = 2000;
-uint32_t DRAM_ATTR offTime = 2000;
-uint32_t DRAM_ATTR brightness = 0;
-uint32_t DRAM_ATTR PWMtimeBrightness;
-
-byte DRAM_ATTR digit[BUFSIZE];
-byte DRAM_ATTR newDigit[BUFSIZE];
-byte DRAM_ATTR oldDigit[BUFSIZE];
-boolean DRAM_ATTR digitDP[BUFSIZE]; // actual value to put to display
-boolean digitsOnly = true;          // only 0..9 numbers are possible to display?
-byte DRAM_ATTR animMask[BUFSIZE];   // 0 = no animation mask is used
-
-byte dayBright = MAXBRIGHTNESS;
-byte nightBright = 5;
 
 void blinkError()
 {
@@ -126,323 +85,87 @@ void startWifi()
   Serial.println(WiFi.RSSI());
 }
 
-void IRAM_ATTR shift(uint32_t Data)
+void displayDigits()
 {
-  static boolean b;
+  digitalWrite(PIN_OE, LOW); // allow data input (Transparent mode)
+  unsigned long var32 = 0;
 
-  for (uint32_t i = 0; i < 32; i++)
-  {
-    digitalWrite(PIN_CLK, HIGH);
-    for (int t = 0; t < 20; t++)
-      asm volatile("nop");
-    if (SHIFT_LSB_FIRST)
-      b = ((Data & (uint32_t(1) << i))) > 0; // LSB first
-    else
-      b = (Data & (uint32_t(1) << (31 - i))) > 0; // MSB first
-    digitalWrite(PIN_DIN, b);
-    for (int t = 0; t < 20; t++)
-      asm volatile("nop");
-    digitalWrite(PIN_CLK, LOW); // falling CLK  to store DIN
-    for (int t = 0; t < 20; t++)
-      asm volatile("nop");
-  }
-  digitalWrite(PIN_CLK, HIGH);
-  for (int t = 0; t < 20; t++)
-    asm volatile("nop");
+  long digits = stringToDisplay.toInt();
+
+  //-------- REG 1 -----------------------------------------------
+  var32 = 0;
+
+  var32 |= (unsigned long)(symbolArray[digits % 10]) << 20; // s2
+  digits /= 10;
+
+  var32 |= (unsigned long)(symbolArray[digits % 10]) << 10; // s1
+  digits /= 10;
+
+  var32 |= (unsigned long)(symbolArray[digits % 10]); // m2
+  digits /= 10;
+
+  SPI.transfer(var32 >> 24);
+  SPI.transfer(var32 >> 16);
+  SPI.transfer(var32 >> 8);
+  SPI.transfer(var32);
+
+  //-------- REG 0 -----------------------------------------------
+  var32 = 0;
+
+  var32 |= (unsigned long)(symbolArray[digits % 10]) << 20; // m1
+  digits = digits / 10;
+
+  var32 |= (unsigned long)(symbolArray[digits % 10]) << 10; // h2
+  digits = digits / 10;
+
+  var32 |= (unsigned long)symbolArray[digits % 10]; // h1
+  digits = digits / 10;
+
+  SPI.transfer(var32 >> 24);
+  SPI.transfer(var32 >> 16);
+  SPI.transfer(var32 >> 8);
+  SPI.transfer(var32);
+
+  digitalWrite(PIN_OE, HIGH); // latching data
 }
 
-void clearTubes()
+void setup()
 {
-  shift(0);
-  shift(0);
-}
-
-void IRAM_ATTR writeDisplay()
-{
-  static DRAM_ATTR byte state = 0;
-  int timer = PWMrefresh;
-
-  intCounter++;
-
-  portENTER_CRITICAL_ISR(&timerMux);
-  noInterrupts();
-  digitalWrite(PIN_OE, LOW); // OFF
-  switch (state)
-  {       // state machine...
-  case 0: // show old character
-    if (time1 >= 1000)
-    {
-      timer = time1;
-      shift(oldBitBuffer1);
-      shift(oldBitBuffer0);
-      state = 1;
-      break;
-    }
-  case 1: // show new character
-    if (time2 >= 1000)
-    {
-      timer = time2;
-      shift(bitBuffer1);
-      shift(bitBuffer0);
-      if (offTime < 500)
-        state = 0;
-      else
-        state = 2;
-      break;
-    }
-  case 2: // blank display
-    timer = offTime;
-    state = 3;
-    break;
-  } // end switch
-
-  if (timer < 500)
-    timer = 500; // safety only...
-
-  if ((state == 3) || (brightness == 0))
-  {                            // OFF state, blank digit
-    digitalWrite(PIN_OE, LOW); // OFF
-    state = 0;
-  }
-  else
-  {                             // ON state
-    digitalWrite(PIN_OE, HIGH); // ON
-  }
-
-  portEXIT_CRITICAL_ISR(&timerMux);
-  timerAlarmWrite(ESP32timer, timer, true);
-  timerAlarmEnable(ESP32timer);
-  interrupts();
-}
-
-void startTimer()
-{
-  //  https://techtutorialsx.com/2017/10/07/esp32-arduino-timer-interrupts/
-  ESP32timer = timerBegin(0, PRESCALER, true); // set prescaler to 80 -> 1MHz signal, true = edge generated signal
-  timerAttachInterrupt(ESP32timer, &writeDisplay, true);
-  timerAlarmWrite(ESP32timer, 1000, true); // 100millisec, no repeat
-  timerAlarmEnable(ESP32timer);
-}
-
-void setup_pins()
-{
-  Serial.println("Setup pins -  HV5122 Nixie driver...");
-  Serial.print("- CLK   : GPIO");
-  Serial.println(PIN_CLK);
-  Serial.print("- DATAIN: GPIO");
-  Serial.println(PIN_DIN);
-  Serial.print("- OUTPUT_ENABLE: GPIO");
-  Serial.println(PIN_OE);
-  pinMode(PIN_CLK, OUTPUT);
-  pinMode(PIN_DIN, OUTPUT);
+  pinMode(PIN_LED, OUTPUT);
   pinMode(PIN_OE, OUTPUT);
-  digitalWrite(PIN_CLK, HIGH);
-  digitalWrite(PIN_OE, LOW);
-  clearTubes();
-  startTimer();
+
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE2);
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
+
+  startWifi();
+
+  initTime("CET-1CEST,M3.5.0,M10.5.0/3");
 }
 
-void writeDisplaySingle()
+void loop()
 {
-  static byte lastAnimMask[BUFSIZE];
-  static byte lastDigit[BUFSIZE];
-  static unsigned long lastRun = 0;
-  uint32_t b0, b1, ob0, ob1; // temp buffers
-  byte num;
-
-  if ((memcmp(digit, lastDigit, maxDigits) == 0) && (memcmp(lastDigit, newDigit, maxDigits) == 0) && (memcmp(animMask, lastAnimMask, maxDigits) == 0))
-    return;
-  // if ((millis()-lastRun)<5) return;
-  lastRun = millis();
-
-  memcpy(lastAnimMask, animMask, maxDigits);
-  memcpy(lastDigit, digit, maxDigits);
-
-  animPtr = 0;
-  b0 = 0;
-  b1 = 0;
-  for (int i = 0; i < maxDigits; i++)
-  { // Set the clock digits new values
-    num = digit[i];
-    if (animMask[i] > 0)
-    {
-      animPtr = animMask[i];
-      num = newDigit[i];
-    }
-    if (num < 10)
-    {
-      if (digitPins[i][num] < 100)
-      {
-        b0 |= (uint32_t)(1 << (digitPins[i][num] - 1));
-        // DPRINT(digitPins[i][num]); DPRINT(":");
-      } // chip0
-      else
-      {
-        b1 |= (uint32_t)(1 << (digitPins[i][num] - 101));
-        // DPRINT(digitPins[i][num]);DPRINT(":");} //chip1
-      }
-    }
-  } // end for i
-
-  if (animPtr > 0)
-  {
-    ob0 = 0;
-    ob1 = 0;
-    for (int i = 0; i < maxDigits; i++)
-    { // Set the clock digits old values
-      num = oldDigit[i];
-      if (num < 10)
-      {
-        if (digitPins[i][num] < 100)
-        {
-          ob0 |= (uint32_t)(1 << (digitPins[i][num] - 1));
-          // DPRINT(digitPins[i][num]); DPRINT(":");
-        } // chip0
-        else
-        {
-          ob1 |= (uint32_t)(1 << (digitPins[i][num] - 101));
-          // DPRINT(digitPins[i][num]);DPRINT(":");} //chip1
-        }
-      }
-    } // end for i
-  }
-
-  for (int i = 0; i < maxDigits - 1; i++)
-  { // Set the extra decimal point dots
-    if (digitDP[i] && digitPins[maxDigits][i] > 0)
-    {
-      if (digitPins[maxDigits][i] < 100)
-      {
-        b0 |= (uint32_t)(1 << (digitPins[maxDigits][i] - 1));  // chip0
-        ob0 |= (uint32_t)(1 << (digitPins[maxDigits][i] - 1)); // chip0
-      }
-      else
-      {
-        b1 |= (uint32_t)(1 << (digitPins[maxDigits][i] - 101));  // chip1
-        ob1 |= (uint32_t)(1 << (digitPins[maxDigits][i] - 101)); // chip1
-      }
-    }
-  } // end for i
-
-  brightness = displayON ? dayBright : nightBright;
-  if (brightness > MAXBRIGHTNESS)
-    brightness = MAXBRIGHTNESS; // only for safety
-
-  if (displayON)
-    PWMtimeBrightness = max(PWM_min, (PWMrefresh * lx) / MAXIMUM_LUX);
-  else
-    PWMtimeBrightness = max(PWM_min, (PWMrefresh * brightness) / MAXBRIGHTNESS);
-
-  if ((animPtr == 0) || (PWMtimeBrightness < 15000))
-  { // no animation #5
-    ob0 = b0;
-    ob1 = b1;
-    animPtr = 10; // to make equal timing
-  }
-
-  time1 = PWMtimeBrightness * (20 - animPtr) / 20;
-  if (time1 < 1000)
-    time1 = 0;
-  if (time1 > (PWMtimeBrightness - 1000))
-    time1 = PWMtimeBrightness;
-  time2 = PWMtimeBrightness - time1;
-  offTime = PWMrefresh - time1 - time2;
-
-  bitBuffer0 = b0;
-  bitBuffer1 = b1;
-  oldBitBuffer0 = ob0;
-  oldBitBuffer1 = ob1;
-}
-
-void displayDate()
-{
-  newDigit[5] = timeInfo.tm_mday / 10;
-  newDigit[4] = timeInfo.tm_mday % 10;
-  newDigit[3] = timeInfo.tm_mon / 10;
-  newDigit[2] = timeInfo.tm_mon % 10;
-  newDigit[1] = (timeInfo.tm_year % 100) / 10;
-  newDigit[0] = timeInfo.tm_year % 10;
-}
-
-void displayTime6()
-{
-  for (int i = 0; i < maxDigits; i++)
-  {
-    digitDP[i] = false;
-  }
-  digitDP[4] = true;
-  digitDP[2] = true;
-  if (showDate)
-  {
-    displayDate();
-  }
-  else
-  {
-    showClock = true;
-    newDigit[5] = timeInfo.tm_hour / 10;
-    newDigit[4] = timeInfo.tm_hour % 10;
-    newDigit[3] = timeInfo.tm_min / 10;
-    newDigit[2] = timeInfo.tm_min % 10;
-    newDigit[1] = timeInfo.tm_sec / 10;
-    newDigit[0] = timeInfo.tm_sec % 10;
-  }
-}
-
-void timeProgram()
-{
-  static unsigned long lastRun = 0;
-  if ((millis() - lastRun) < 200)
-    return;
-  lastRun = millis();
   if (!getLocalTime(&timeInfo))
   {
     Serial.println("Failed to obtain time");
     blinkError();
     return;
   }
-  if (timeInfo.tm_sec != prevSecond || timeInfo.tm_min != prevMinute || timeInfo.tm_hour != prevHour)
+  time_t nowTime = mktime(&timeInfo);
+  if (nowTime != prevTime)
   { // Update time every second
-    prevSecond = timeInfo.tm_sec;
-    prevMinute = timeInfo.tm_min;
-    prevHour = timeInfo.tm_hour;
-    showDate = (timeInfo.tm_sec >= 50) && (timeInfo.tm_sec < 55);
-    displayTime6();
-    changeDigit();
-    printDigits(0);
-    if (!initProtectionTimer && (timeInfo.tm_min == 0)) // we never ran protection yet
+    prevTime = nowTime;
+    if ((timeInfo.tm_sec >= 50) && (timeInfo.tm_sec < 55))
     {
-      protectTimer = 0; // Ensures protection logic will be immediately triggered
-      initProtectionTimer = true;
+      displayDate();
     }
-
-    if ((mktime(&timeInfo) - protectTimer) >= 60 * 15)
+    else
     {
-      protectTimer = mktime(&timeInfo);
-      // The current time can drift slightly relative to the protectTimer when NIST time is updated
-      // Need to make a small adjustment to the timer to ensure it is triggered at the minute change
-      protectTimer -= ((timeInfo.tm_sec + 30) % 60 - 30);
-      if (millis() > 50000)
-      {
-        newCathodeProtect(maxDigits * 1500, random(3) - 1); // dont play in the first 50sec
-      }
+      displayTime();
     }
   }
-}
-
-void setup()
-{
-  pinMode(PIN_LED, OUTPUT);
-  setup_pins();
-
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  startWifi();
-
-  initTime("CET-1CEST,M3.5.0,M10.5.0/3");
-  printLocalTime();
-}
-
-void loop()
-{
-  timeProgram();
+  delay(200);
 }
